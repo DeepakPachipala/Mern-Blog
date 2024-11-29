6b8ac01df4ba8eff003c1ef1144b8b7f6b585449
DeepakPachipala <deepakpachipala@gmail.com>
2024-11-29 12:16:46 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
-const express = require('express')
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();  
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Db Connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+   
+  });
 
 const app = express();
 
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    
-})
+  console.log("Server is running on port 3000");
+});
